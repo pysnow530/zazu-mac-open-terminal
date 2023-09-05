aa184c4d33611831783e84f5ec528d5932ca458c
oogwu <oogwu@tencent.com>
2023-09-05 17:30:05 +0800

    Fix blocks name

--- zazu.js
+++ zazu.js
@@ -4,7 +4,7 @@ module.exports = {
   "version": "1.0.1",
   "description": "Open terminal here.",
   "blocks": {
-    "external": [
+    "input": [
       {
         "id": "OpenTerminalKey",
         "type": "Keyword",
