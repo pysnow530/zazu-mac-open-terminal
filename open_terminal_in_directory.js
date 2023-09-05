c2265c28a257eaecac3f2d000cf690b054329d7c
pysnow530 <pysnow530@163.com>
2023-09-05 20:46:35 +0800

    Fix scripts

--- open_terminal_in_directory.js
+++ open_terminal_in_directory.js
@@ -1,23 +1,7 @@
-const exec = require('child_process').exec
-const path = require('path')
-
 module.exports = (pluginContext) => {
-  const appleScriptTerminal = path.join(pluginContext.cwd, 'open_terminal_in_directory.scpt')
-  const appleScriptITerm2 = path.join(pluginContext.cwd, 'open_iterm2_in_directory.scpt')
-  const cmd = {
-    terminal: `osascript ${appleScriptTerminal}`,
-    iterm2: `osascript ${appleScriptITerm2}`,
-  }
-  return (dir, env) => {
-    const command = (cmd[env.terminal] || cmd.terminal) + ' ' + dir
-    return new Promise((resolve) => {
-      resolve([
-        {
-          icon: 'fa-terminal',
-          title: `Open term in ${dir}`,
-          value: dir
-        }
-      ])
+  return (command, env = {}) => {
+    return new Promise((resolve, reject) => {
+      exec(command)
     })
   }
 }
